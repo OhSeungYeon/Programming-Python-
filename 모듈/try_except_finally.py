ef5bfd56e25ead21b03a3f7756347ba182110272
OSY <dhtmddus55@naver.com>
2019-09-23 14:19:43 +0900

    예외 처리

---
+++ 모듈/try_except_finally.py
@@ -0,0 +1,12 @@
+f = open("file.txt", "w")
+
+try :
+    f.wirte("Hello World!")
+    f.write(100)
+
+except TypeError :
+    print("타입 예외 발생(100은 할 수 없음)")
+
+finally :
+    print("예외 여부와 상관없이 무조건 실행")
+    f.close()
