1ec337f426ea328bf015d31721ae334f3fb51373
OSY <dhtmddus55@naver.com>
2019-11-25 09:35:29 +0900

    web crawling 연애혁명 제목, 링크 가져오자

--- IX/web_crawling.py
+++ IX/web_crawling.py
@@ -8,10 +8,14 @@ if __name__ == '__main__':
     #네이버웹툰 > 연애혁명 제목 가져오자
     data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=570503")
     soup = BeautifulSoup(data, "lxml")
-    cartoon_titles = soup.find_all("td", attrs={"class":"title"})
+    cartoon_titles = soup.find_all("td", attrs={"class":"title"}) #td class = "title"...
     for cartoon_title in cartoon_titles :
+    #for cartoon_title in cartoon_titles(:2) :
         #print(cartoon_title)
         #리스트에다가 하나씩 넣었다가 넣은 것을 하나씩 출력
         title = cartoon_title.find("a").text
+        link = cartoon_title.find("a").get("href")
+        link = "https://comic.naver.com" + link
         print(title)
+        print(link)
     
