84969ac35b6e7ad4b3425a5c16dc2d31db16ad6c
OSY <dhtmddus55@naver.com>
2019-12-09 08:42:20 +0900

    pyqutogui - keyboard

---
+++ IX/macro2.py
@@ -0,0 +1,12 @@
+
+import pyautogui as pag
+import time #sleep
+
+if __name__ == '__main__':
+    pag.moveTo(200, 200, 2)
+    pag.click("right")
+    pag.typewrite("happy birth day to seungyeon")
+    pag.press("enter")
+    pag.typewrite("happy birth day to namkyeoung")
+    pag.press("hangul")
+    pag.typewrite("skaruddk toddlf cnrgkgo!!")
